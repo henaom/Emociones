ad8335a7990272fec53b9218bc0b2ae2f9853a6c
henaom <160857383+henaom@users.noreply.github.com>
2024-03-14 07:44:23 -0500

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from PIL import Image
 from googletrans import Translator
 
 st.title('Análisis de Sentimiento')
-image = Image.open('emoticones.jpg')
+image = Image.open('Colores-y-emociones-1.jpg')
 st.image(image)
 st.subheader("Por favor escribe en el campo de texto la frase que deseas analizar")
 
@@ -23,7 +23,12 @@ with st.expander('Analizar texto'):
         x=round(blob.sentiment.polarity,2)
         if x >= 0.5:
             st.write( 'Es un sentimiento Positivo 😊')
+            st.write( '💙Las emociones y los pensamientos positivos nos ayudan a abrirnos a nuevas posibilidades.
+            Tenemos más capacidad para aprender y desarrollar nuestras habilidades 💙.')
         elif x <= -0.5:
             st.write( 'Es un sentimiento Negativo 😔')
+            st.write(' A veces no es fácil lidiar con ellos, pero lo cierto es que ahí están, todos los sentimos. 
+            Lo mejor para tu salud emocional es afrontarlos y tratar de superarlos con toda la paciencia del mundo.😀')
         else:
             st.write( 'Es un sentimiento Neutral 😐')
+            st.write('Todo en la vida pasa 😋')
